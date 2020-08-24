4e260083a0ab81d0c00a4e1b11cf82e0dad7d4ee
sugiyama <fwkym471blue@gmail.com>
2020-08-24 23:00:56 +0900

    各カテゴリーをデータベースに格納

--- init_db.py
+++ init_db.py
@@ -6,8 +6,8 @@ c = conn.cursor()
 
 # テーブル作成
 c.execute("create table users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT , password TEXT)")
-c.execute("create table k_posts(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, comment TEXT, del_flag INTEGER, time TEXT)")
-
+c.execute("create table k_posts(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, comment TEXT, del_flag INTEGER, time TEXT, cat_id INTEGER)")
+c.execute("create table categories(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
 
 # 確定
 conn.commit()
